7050f4cd020212813c71e92fc14c4f1ed23b3bf0
Istiak <aistiak2078@gmail.com>
2021-11-21 18:05:26 +0600

    ad API resolved

--- lib/services/api/ad.dart
+++ lib/services/api/ad.dart
@@ -2,22 +2,22 @@ import 'dart:io';
 
 import 'package:flutter/cupertino.dart';
 import 'package:http/http.dart' as http;
+import 'package:winly/services/api/api_service.dart';
 import 'package:winly/services/api/url.dart';
 
 class AdAPI {
   static Future<http.Response?> requestForTicket(
-      {required String adStatus}) async {
+      {required String adStatus, String? token}) async {
     try {
       final url = urlBuilder('api/user-ad');
 
-      final response = await http.post(
-        Uri.parse(url),
-        body: {
-          'ad-status': adStatus,
-        },
-        headers: commonHeader(),
-      );
-      debugPrint(response.body);
+      final response = await http.post(Uri.parse(url), body: {
+        'ad_status': adStatus,
+      }, headers: {
+        "Accept": "application/json",
+        'Authorization': 'Bearer $token'
+      });
+      // debugPrint(response.body);
       return Future.value(response);
     } on SocketException catch (_) {
       return null;
