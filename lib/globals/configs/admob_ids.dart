31c8aac6cf0d607fd4ff55b95f0391875c1466d8
mazharulsabbir <mazharulsabbir2@gmail.com>
2021-12-27 23:07:39 +0600

    release version 1.1.1

--- lib/globals/configs/admob_ids.dart
+++ lib/globals/configs/admob_ids.dart
@@ -1,10 +1,5 @@
 import 'package:flutter/foundation.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 
-String interstitialAdUnitId = !kReleaseMode
-    ? InterstitialAd.testAdUnitId
-    : "ca-app-pub-2245972702677610/3391545597";
-
-String rewardVideoAdUnitId = !kReleaseMode
-    ? RewardedAd.testAdUnitId
-    : "ca-app-pub-2245972702677610/5796951737";
+const String interstitialAdUnitId = "ca-app-pub-2245972702677610/3391545597";
+const String rewardVideoAdUnitId = "ca-app-pub-2245972702677610/5796951737";
